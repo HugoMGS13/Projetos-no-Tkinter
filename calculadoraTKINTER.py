c4a3e7d8934e4979dc0a22d5723ba71b36744788
HugoMGS13 <127580302+HugoMGS13@users.noreply.github.com>
2023-04-07 16:27:41 -0300

    Update calculadoraTKINTER.py

--- calculadoraTKINTER.py
+++ calculadoraTKINTER.py
@@ -2,8 +2,9 @@ from tkinter import *
 import math
 janela = Tk()
 janela.title("Calculadora")
-janela.geometry("420x260")
-janela.configure(background="#dde")
+janela.geometry("420x300")
+janela.resizable(False,False)
+janela.configure(background="#00BFFF")
 def btmais():
     global x
     x = int(nmr1.get())+int(nmr2.get())
@@ -28,12 +29,12 @@ def limpar():
     nmr1.delete(0,END)
     nmr2.delete(0,END)
     resp["text"]=""
-txt1 = Label(janela,text="Primeiro número")
-txt1.grid(column=0,row=0)
-nmr1=Entry(janela)
+txt1 = Label(janela,text="Primeiro número",bg="#00BFFF")
+txt1.grid(column=0,row=0,pady=0)
+nmr1=Entry(janela) #Cria uma janela interagível, like "input()"
 nmr1.grid(column=0,row=1)
-txt2 = Label(janela,text="Segundo número")
-txt2.grid(column=2,row=0,padx=0)
+txt2 = Label(janela,text="Segundo número",bg="#00BFFF")
+txt2.grid(column=2,row=0,padx=0,pady=0)
 nmr2=Entry(janela) #Cria uma janela interagível, like "input()" ; o Text() é a mesma coisa do Entry() porém com mais linhas
 nmr2.grid(column=2,row=1,pady= 10)
 btt1 = Button(janela,text="=",command=btresult)
@@ -54,6 +55,10 @@ resp=Label(janela,text="",background='#fff')
 resp.grid(column=1,row=3)
 dele = Button(janela, text="Limpar",command=limpar)
 dele.grid(column=1,row=4)
-atencao=Label(janela,text="1º - Digite os números\n2º - Selecione a operação\n3º - Clique para ver o resultado")
+atencao=Label(janela,text="1º - Digite os números\n2º - Selecione a operação\n3º - Clique para ver o resultado",bg="#00BFFF")
 atencao.grid(column=1,row=5)
+logo = PhotoImage(file="calcu.png")
+logo = logo.subsample(7,7)
+fot = Label(image=logo,bg="#00BFFF")
+fot.grid(column=1,row=1,pady=0)
 janela.mainloop()
